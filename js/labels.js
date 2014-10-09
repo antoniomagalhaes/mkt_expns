2e2586a046910000e2582032cef3493ad7d955df
António Mário Costa de Magalhaes <contacto@antoniomagalhaes.com>
2014-10-09 23:48:45 +0100

    início de projecto

---
+++ js/labels.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function($) {
+
+    if (labelPage === 0) {
+        $('#HomePageTitle').html(HomePage.Title);
+        $('#HomePageTitle1').html(HomePage.Title1);
+        $('#HomePageSubtitle').html(HomePage.Subtitle);
+        $('#HomePageContent').html(HomePage.Content);
+        $('#HomePageStartButton').html(HomePage.StartButton);
+        $('#HomePageHeader1').html(HomePage.Header1);
+        $('#HomePageContent1').html(HomePage.Content1);
+        $('#HomePageHeader2').html(HomePage.Header2);
+        $('#HomePageContent2').html(HomePage.Content2);
+        $('#HomePageHeader3').html(HomePage.Header3);
+        $('#HomePageContent3').html(HomePage.Content3);
+
+        $('#HomePageCompanyName').html(HomePage.CompanyName);
+        $('#HomePageAddress1').html(HomePage.Address1);
+        $('#HomePageAddress2').html(HomePage.Address2);
+        $('#HomePageAddress3').html(HomePage.Address3);
+
+        $('#HomePageDisclaimer').html(HomePage.Disclaimer);
+        $('#HomePagePrivacyStatement').html(HomePage.PrivacyStatement);
+
+        $('#HomePageContactUs').html(HomePage.ContactUs);
+        $('#HomePageAbout').html(HomePage.About);
+
+        //$('#LandingBackground').html(HomePage.About);
+    }
+    if (labelPage === 1) {
+//VEICULO - Tipo\\Prazo\\KMs\\Financiamento
+        $('#HomePageTitle').html(HomePage.Title);
+        $('#HomePageTitle1').html(HomePage.Title1);
+        $('#ComparisonPageSelectVehicle').html(ComparisonPage.SelectVehicle);
+        $('#ComparisonPageTypeofCar').html(ComparisonPage.TypeofCar);
+        $('#ComparisonPageFinanceMethod').html(ComparisonPage.FinanceMethod);
+        $('#ComparisonPageLeaseYears').html(ComparisonPage.LeaseYears);
+        $('#ComparisonPageAnnualMileage').html(ComparisonPage.AnnualMileage);
+
+        $('#ComparisonPageBankLoan').html(ComparisonPage.BankLoan);
+        $('#ComparisonPageFinanceLease').html(ComparisonPage.FinanceLease);
+        $('#ComparisonPageOwnFunds').html(ComparisonPage.OwnFunds);
+
+        $('#ComparisonPageMake').html(ComparisonPage.Make);
+        $('#ComparisonPageModel').html(ComparisonPage.Model);
+        $('#ComparisonPageType').html(ComparisonPage.Type);
+        $('#ComparisonPageFueltype').html(ComparisonPage.Fueltype);
+        $('#ComparisonPageListPrice').html(ComparisonPage.ListPrice);
+
+        $('#ComparisonPageTailoredReport').html(ComparisonPage.TailoredReport);
+        $('#ComparisonPageCallUs').html(ComparisonPage.CallUs);
+        $('#ComparisonPageContactUs').html(ContactPage.Title);
+        $('#ComparisonPageInfoPricesPictures').html(ComparisonPage.InfoPricesPictures);
+
+        $('#ComparisonPageFinanceMethodCaps').html(ComparisonPage.FinanceMethod2);
+        $('#ComparisonPageTotalMonthlyCosts').html(ComparisonPage.TotalMonthlyCosts);
+        $('#ComparisonPageTotalSavings').html(ComparisonPage.TotalSavings);
+        $('#ComparisonPageAllpricesVATexcl').html(ComparisonPage.AllpricesVATexcl);
+        $('#ComparisonPagePrintButton').html(ComparisonPage.Print);
+
+        $('#ComparisonPageDetailsCostbreakdown').html(ComparisonPage.DetailsCostbreakdown);
+
+        $('.ComparisonPageYourMonthlyCost').html(ComparisonPage.YourMonthlyCost);
+        $('.ComparisonPageOurLeaseprice').html(ComparisonPage.OurLeaseprice);
+        $('.ComparisonPageHowCalculated').html(ComparisonPage.HowCalculated);
+        $('.ComparisonPageHowSavings').html(ComparisonPage.HowSavings);
+        $('.ComparisonPageVatExcluded').html(ComparisonPage.VatExcluded);
+
+        $('.ComparisonPageOpen').html(ComparisonPage.Open);
+        $('.ComparisonPageClose').html(ComparisonPage.Close);
+
+        $('#ContactPageTitle').html(ContactPage.Title);
+        $('#ContactPageName').html(ContactPage.Name);
+        $('#ContactPageEmail').html(ContactPage.Email);
+        $('#ContactPageMr').html(ContactPage.Mr);
+        $('#ContactPageMrs').html(ContactPage.Mrs);
+        $('#ContactPagePhonenumber').html(ContactPage.Phonenumber);
+        $('#ContactPageNumberVehicles').html(ContactPage.NumberVehicles);
+        $('#ContactPageTailoredReport').html(ContactPage.TailoredReport);
+        $('#ContactPageYesOption').html(ContactPage.YesOption);
+        $('#ContactPageNoOption').html(ContactPage.NoOption);
+
+        $('#ContactPageExtraField1').html(ContactPage.ExtraField1);
+        $('#ContactPageExtraField2').html(ContactPage.ExtraField2);
+        $('#ContactPageExtraField3').html(ContactPage.ExtraField3);
+        $('#ContactPageExtraField4').html(ContactPage.ExtraField4);
+        $('#ContactPageExtraField5').html(ContactPage.ExtraField5);
+        $('#ContactPageExtraField6').html(ContactPage.ExtraField6);
+        $('#ContactPageExtraField7').html(ContactPage.ExtraField7);
+        $('#ContactPageExtraField8').html(ContactPage.ExtraField8);
+
+        $('#ContactPageCloseButton').html(ContactPage.Close);
+        $('#ContactPageSend').html(ContactPage.Send);
+
+        $('#CarFinanceTitle').html(CarFinance.Title);
+        $('#CarFinanceMontlyCostInfo').html(CarFinance.MontlyCostInfo);
+        $('#CarFinanceOurSavingsInfo').html(CarFinance.OurSavingsInfo);
+
+        $('#RunningCostsTitle').html(RunningCosts.Title);
+        $('#RunningCostsMontlyCostInfo').html(RunningCosts.MontlyCostInfo);
+        $('#RunningCostsOurSavingsInfo').html(RunningCosts.OurSavingsInfo);
+
+        $('#DowntimeCostsTitle').html(DowntimeCosts.Title);
+        $('#DowntimeCostsMontlyCostInfo').html(DowntimeCosts.MontlyCostInfo);
+        $('#DowntimeCostsOurSavingsInfo').html(DowntimeCosts.OurSavingsInfo);
+
+        $('#ManagementCostsTitle').html(ManagementCosts.Title);
+        $('#ManagementCostsMontlyCostInfo').html(ManagementCosts.MontlyCostInfo);
+        $('#ManagementCostsOurSavingsInfo').html(ManagementCosts.OurSavingsInfo);
+
+        $('#UsedCarSaleIncomeTitle').html(UsedCarSaleIncome.Title);
+        $('#UsedCarSaleIncomeMontlyCostInfo').html(UsedCarSaleIncome.MontlyCostInfo);
+        $('#UsedCarSaleIncomeOurSavingsInfo').html(UsedCarSaleIncome.OurSavingsInfo);
+
+        $('#ComparisonPageHowCalculated').html(ComparisonPage.YourMonthlyIncome);
+
+        
+    }
+});
+
---
+++ process.php
@@ -0,0 +1,207 @@
+<?php 
+/*
+ * BEGIN CONFIG
+ */
+
+// The page you want the user to be redirected if there are no errors.
+$thankYouPage = 'thanks.html';
+
+// Define which values we are to accept from the form. If you add additional 
+// fields to the form, make sure to add the form nome values here.
+$allowedFields = array(
+	'nome', 
+	'localidade',
+	'email',
+	'telefone',
+	'agree',
+	//'newsletter',
+	//'topic',
+);
+
+// Specify the required form fields. The key is the field nome and the value 
+// is the error message to display.
+$requiredFields = array(
+	'nome' => 'Insira o seu Nome.', 
+	'localidade' => 'Insira a sua localidade.',
+	'email' => 'Insira o seu email.',
+	//'telefone' => 'telefone number is required.',
+	//'agree' => 'You must agree with our policy.',
+);
+
+// Note: Since we are requiring two topics to be checked if they want to receive 
+// our newsletter, we need to implement custom code.
+
+/*
+ * END CONFIG
+ */
+
+
+/*
+ * Since we are doing javascript error checking on the form before the form gets submitted, 
+ * the server side check is only a backup if the user has javascript disabled. Since this 
+ * is unlikely, we are displaying the server side errors on a separate page of the form.
+ * 
+ * The more practical purpose of server side checking (in this case) is to prevent hackers from exploiting 
+ * your PHP processing code.
+ */
+
+
+/*
+ * BEGIN FORM VALIDATION
+ */
+
+$errors = array();
+
+// We need to loop through the required variables to make sure they were posted with the form.
+foreach($requiredFields as $fieldnome => $errorMsg)
+{
+	if(empty($_POST[$fieldnome]))
+	{
+		$errors[] = $errorMsg;
+	}
+}
+
+// Loop through the $_POST array, to create the PHP variables from our form.
+foreach($_POST AS $key => $value)
+{
+    // Is this an allowed field? This is a security measure.
+    if(in_array($key, $allowedFields))
+    {
+        ${$key} = $value;
+    }
+}
+
+// Code to validate the newsletter topic checkboxes
+//if(!empty($_POST['newsletter']))
+{
+	// They checked the newsletter checkbox...make sure they 
+	// checked at least two topics.
+	//if(count($_POST['topic']) < 2)
+	{
+		//$errors[] = "In order to receive our newsletter, you must check at least two topics.";
+	}
+}
+
+/*
+ * END FORM VALIDATION
+ */
+
+
+// Were there any errors?
+if(count($errors) > 0)
+{
+    $errorString .= '<ul>';
+    foreach($errors as $error)
+    {
+        $errorString .= "<li>$error</li>";
+    }
+    $errorString .= '</ul>';
+ 
+    // display the errors on the page
+    ?>
+    <html>
+    <head>
+    <title>Erro no processamento do Formulário</title>
+    </head>
+    <body>
+    <h2>Erro no processamento do Formulário</h2>
+    <p>Houve um erro no processamento do Formulário!</p>
+    <?php echo $errorString; ?>
+    <p><a href="index.php">Voltar ao formulário!</a></p>
+    </body>
+    </html>
+    <?php 
+}
+else
+{
+    // At this point you can send out an email or do whatever you want
+    // with the data...
+ 
+    // each allowed form field nome is now a php variable that you can access
+ 
+ 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$localidade = $_POST['localidade'];
+$telefone = $_POST['telefone'];
+
+$subject = "Simulador Market Expanssion LeasePlan GO" ;
+
+$messageBody = "
+Agradecemos o seu interesse e queremos ajudá-lo optimizar a sua frota .
+Abaixo enviamos os dados por si submetidos.
+
+Nome: ".$nome."
+Email: ".$email."
+Localidade: ".$localidade."
+Telefone: ".$telefone."
+
+Muito obrigado.
+";
+
+// send a copy to the lister
+$recipientemail=$email;
+$firstLineOfemail = $subject;
+$messageBody = $firstLineOfemail . $messageBody;
+$systememail = "antonio.magalhaes@stepvalue.com";
+$from = "From: $systememail\n"; /* used as the 4th mail() argument */
+$replyTo = "Reply-To: $email\n";
+$recipient = $recipientemail;
+$xMailer = "X-Mailer: PHP/" . phpversion();
+$optionalHeaders = $from . $replyTo . $xMailer;
+$customer_emailed = "NO";
+
+if( mail( $recipient, $subject, $messageBody, $optionalHeaders ) ) {
+$customer_emailed = "YES";
+}
+
+$subject = "Simulador Market Expanssion";
+
+$messageBody = "
+" . $nome . "Pedido de Relatório - Prospect com os seguintes dados:
+
+nome: ".$nome."
+email: ".$email."
+localidade: ".$localidade."
+Telefone: ".$telefone."
+
+,
+
+";
+
+// Send to email j
+
+$recipientemail= "contacto@antpniomagalhaes.com";
+
+$reg_emailed = "NO";
+
+ini_set('sendmail_from', 'contacto@antpniomagalhaes.com');
+
+if (mail( $recipientemail, $subject, $messageBody, $optionalHeaders )){
+
+$reg_emailed = "YES";
+
+}
+
+if(($reg_emailed == "YES"))
+{
+//$msg = "
+//
+//Thanks for filling out our Quote Request Form
+//Click the X to close this window
+//"
+;
+
+} else {
+
+$msg = "
+Não nos foi possível processar os seus dados.
+Por favor tente mais tarde.
+Muito obrigado pelo seu interesse.";
+}
+
+ 
+    // display the thank you page
+    header("Location: $thankYouPage");
+}
+?>
